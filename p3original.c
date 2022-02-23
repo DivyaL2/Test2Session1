9acecabe8d94bbc11a0d5322385b922b4c21633e
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-02-23 04:59:47 +0000

    done the code

--- p3original.c
+++ p3original.c
@@ -1,5 +1,34 @@
 #include<stdio.h>
 int input_number()
 {
-   printf("enter any number\n")
+   int n;
+   printf("enter any number\n");
+   scanf("%d",&n);
+   return n;
 }
+int is_prime(int n);
+{
+  int count=0;
+  for(int i=0;i<n;i++)
+  {
+     if(n%2==0)
+     count ++; 
+  }
+}
+return count;
+void output(int n, int count)
+{
+  if(count<2)
+  {
+    printf("%d is a prime number\n",n);
+  }
+  else
+  {
+    printf("%d is not a prime number\n",n);
+    
+  }
+}
+
+  
+    
+
