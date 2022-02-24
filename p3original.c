eecce89ebbe99fcbbd95d8defeaeffa6f6cf4fd7
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-02-24 15:22:32 +0000

    done the code of prime no

--- p3original.c
+++ p3original.c
@@ -6,27 +6,34 @@ int input_number()
    scanf("%d",&n);
    return n;
 }
-int is_prime(int n);
+int is_prime(int n)
 {
   int count=0;
-  for(int i=0;i<n;i++)
+  for(int i=1;i<=n;i++)
   {
-     if(n%2==0)
+     if(n%i==0)
      count ++; 
   }
+   return count;
 }
-return count;
-void output(int n, int count)
+void output(int n, int is_prime)
 {
-  if(count<2)
+  if(is_prime>2)
   {
-    printf("%d is a prime number\n",n);
+    printf("%d is not a prime number\n",n);
   }
   else
   {
-    printf("%d is not a prime number\n",n);
+    printf("%d is a prime number\n",n);
     
   }
+}
+int main()
+{
+  int n,prime;
+  n=input_number();
+  prime=is_prime(n);
+  output(n,prime);
 }
 
   
