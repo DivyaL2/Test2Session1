5790ecb4f19c63c1304e00798e57b956a721f3e4
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-03-05 05:45:44 +0000

    done the code of prime numbers

--- p3original.c
+++ p3original.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 int input_number()
 {
    int n;
@@ -8,23 +9,22 @@ int input_number()
 }
 int is_prime(int n)
 {
-  int count=0;
-  for(int i=1;i<=n;i++)
+  for(int i=2;i<=sqrt(n);i++)
   {
      if(n%i==0)
-     count ++; 
+     return 0;
   }
-   return count;
+   return 1;
 }
 void output(int n, int is_prime)
 {
-  if(is_prime>2)
+  if(is_prime)
   {
-    printf("%d is not a prime number\n",n);
+    printf("%d is  a prime number\n",n);
   }
   else
   {
-    printf("%d is a prime number\n",n);
+    printf("%d is not a prime number\n",n);
     
   }
 }
